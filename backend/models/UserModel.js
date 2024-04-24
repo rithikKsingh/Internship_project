bad6abe460a70772a4f571ad52cfcdfbaf9430d1
Rithik Kumar Singh <112690734+rithikKsingh@users.noreply.github.com>
2024-04-24 14:56:53 +0530

    Update UserModel.js

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -16,21 +16,27 @@ const userSchema = mongoose.Schema({
   },
   phoneNumber: {
     type: String,
+    required: true,    
   },
   address: {
     type: String,
+    required: true,    
   },
   country: {
     type: String,
+    required: true,
   },
   zipCode: {
     type: String,
+    required: true,    
   },
   city: {
     type: String,
+    required: true,    
   },
   state: {
     type: String,
+    required: true,    
   },
   password: {
     type: String,
