f43e11613147a048d437a12adf5bdf7da13e6652
Rithik Kumar Singh <112690734+rithikKsingh@users.noreply.github.com>
2024-04-26 17:54:51 +0530

    Add files via upload

---
+++ frontend/src/pages/admin/components/HomePageComponent.test.js
@@ -0,0 +1,33 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import HomePageComponent from "./HomePageComponent";
+import { MemoryRouter as Router } from "react-router-dom";
+
+let categories = [];
+let getBestsellers = () => {
+  return new Promise((resolve, reject) => {
+    resolve([
+      {
+        _id: "65e43f53c82b1eb4996c2188",
+        name: "Product1 Lenovo Comp1 Name Lorem ipsum dolor sit amet",
+        description:
+          "Product Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ipsa ducimus architecto explicabo id accusantium nihil exercitationem autem porro esse.",
+        category: "Computers/Laptops/Lenovo",
+        images: [{ path: "path" }],
+      },
+    ]);
+  });
+};
+
+test("if category is seen", async () => {
+  render(
+    <Router>
+      <HomePageComponent
+        categories={categories}
+        getBestsellers={getBestsellers}
+      />
+    </Router>
+  );
+  await waitFor(() => screen.getByText(/Computers\/Laptops\/Lenovo/i));
+  expect(screen.getByText(/Computers\/Laptops\/Lenovo/i)).toBeInTheDocument();
+});
