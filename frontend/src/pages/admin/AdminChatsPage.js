adb8bfa4571bbf6cab4866f1e0f6ce6d130f5b01
Rithik Kumar Singh <112690734+rithikKsingh@users.noreply.github.com>
2024-04-29 12:36:27 +0530

    Update AdminChatsPage.js

--- frontend/src/pages/admin/AdminChatsPage.js
+++ frontend/src/pages/admin/AdminChatsPage.js
@@ -2,23 +2,30 @@ import AdminLinksComponent from "../../components/admin/AdminLinksComponent";
 import AdminChatRoomComponent from "../../components/admin/AdminChatRoomComponent";
 import { Row, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
+
 const AdminChatsPage = () => {
-  const {chatRooms,socket}=useSelector((state)=>state.adminChat)
-  
-  return (
-    <Row className="m-5">
-      <Col md={2}>
-        <AdminLinksComponent />
-      </Col>
-      <Col md={10}>
-        <Row>
-          {Object.entries(chatRooms).map((chatRoom,index)=>( 
-          <AdminChatRoomComponent key={index} chatRoom={chatRoom} roomIndex={index+1} socket={socket} socketUser={chatRoom[0]}/>
-          ))}
+    const { chatRooms, socket } = useSelector((state) => state.adminChat);
+
+    return (
+        <Row className="m-5">
+            <Col md={2}>
+                <AdminLinksComponent />
+            </Col>
+            <Col md={10}>
+                <Row>
+                    {Object.entries(chatRooms).map(([socketUser, chatRoom], index) => (
+                        <AdminChatRoomComponent
+                            key={socketUser}
+                            chatRoom={chatRoom}
+                            roomIndex={index + 1}
+                            socket={socket}
+                            socketUser={socketUser}
+                        />
+                    ))}
+                </Row>
+            </Col>
         </Row>
-      </Col>
-    </Row>
-  );
+    );
 };
 
 export default AdminChatsPage;
